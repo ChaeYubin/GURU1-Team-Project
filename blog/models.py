1cd57b18e26bbf5e437f23305b7f23b27a6e91b4
Song Jeongyeon <songjjing@naver.com>
2020-08-14 05:48:41 +0900

    group page level.0

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from markdownx.models import MarkdownxField
 
 
 class Post(models.Model):
-    objects = None
+
     title = models.CharField(max_length=30)
     content = MarkdownxField()
     head_image = models.ImageField(upload_to='blog/%y/%m/%d/', blank=True)
@@ -12,4 +12,4 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.name
+        return '{} :: {}'.format(self.title, self.author)
