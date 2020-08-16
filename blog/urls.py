313403002e0b66831e628c063958f7ae8208327a
Hojung <67528774+hojung000@users.noreply.github.com>
2020-08-17 00:54:51 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import path
 from .import views
 
-app_name = "users"
+app_name = "blog"
 
 urlpatterns = [
     path('category/<str:slug>/', views.PostListByCategory.as_view()),
